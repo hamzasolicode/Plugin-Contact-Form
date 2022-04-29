2b55ece50860e5780872e33a14be0844b5e5c207
blq200 <92025169+blq200@users.noreply.github.com>
2022-04-29 10:08:46 +0000

    Add files via upload

---
+++ formcontact/admin/admin.php
@@ -0,0 +1,118 @@
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+            
+            <h1 style="text-align:center;">CONTACT FORM</h1>
+            <br><br>
+            <section  style=" display:flex ;justify-content:center ; ">
+            
+            <form action="#" style=" display:flex ;justify-content:center ;flex-direction:column; " method="POST">
+            
+                <label for="Name">UserName</label>
+            
+                <br><input  type="text" placeholder="Enter Your UserName" style="width:300px" name="username">
+
+                <br><label for="email">Email</label>
+                <br><input type="email" placeholder="Enter Your Email" style="width:300px" name="email">
+
+                <br><label for="number">Phone Number</label>
+                <br><input type="tél" placeholder="Enter Your Phone Number" style="width:300px" name="phone_number">
+
+                <br><label for="comments">Questions/Comments</label>
+                <br><textarea name="Your_Comments" placeholder="Enter Your Questions Or Your Comments"style="width:300px" name="comment" ></textarea>
+                <br>
+                <!-- <input type="submit" name="edit_form" class="btn btn-dark" value="SAVE"style="width:300px" /> -->
+                
+            </form>
+            <section style="display:flex;flex-direction:column ;margin: 6rem;padding:6em">
+            <form action="#" method="POST">
+            <label for="UserName">UserName</label>
+                <input type="radio"name="name" value="name"><br><br>
+                <label for="Email">Email</label>
+                <input type="radio" name="email" value="email"  ><br><br>
+                <label for="Phone Number">Phone Number</label>
+                <input type="radio"name="number"value="nmber" ><br><br>
+                <label for="Questions/Comments">Questions/Comments</label>
+                <input type="radio"name="Comments"value="Comments" ><br><br>
+                <input name="submit" type="submit" value="submit" class="btn btn-dark" style="width:300px" >
+                </form>
+                
+                </section>
+        
+            </section>
+
+        <?php
+
+
+
+            if ( isset( $_POST['name'] ) )
+            {$name= true;}
+        else
+        { 
+            $name= false;
+        }
+
+            if(isset( $_POST['email'] ) )
+            {$email= true;}
+        else
+        { 
+            $email= false;
+        }
+
+            if(isset( $_POST['number'] ) )
+            {$number= true;}
+        else
+        { 
+            $number= false;
+        }
+
+            if(isset( $_POST['Comments'] ) )
+            {$Comments= true;}
+        else
+        { 
+            $Comments= false;
+        }
+
+            $data= array('name','email','number','Comments');
+            $vari= array($name, $email, $number, $Comments);
+            for($i=0; $i<count($data);$i++)
+            {
+                if ( isset( $_POST['submit'] ) ){
+            update_option($data[$i],$vari[$i]);
+        }
+
+            }
+
+            ?>
+
+
+        <?php
+
+        if ( isset( $_POST['submit'] ) ){
+
+            global $wpdb;
+
+
+            $tablename=$wpdb->prefix.'wp_options';
+
+            $data=array(
+                'option_name' => $_POST['box-name'], 
+                'option_value' => 'true'
+            );
+
+
+            $wpdb->insert( $tablename, $data);
+        }
+
+        ?>
+        
+        </body>
+        </html>
---
+++ formcontact/form.php
@@ -0,0 +1,92 @@
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+            <title>Document</title>
+        </head>
+        <body>
+            
+        </body>
+        </html>
+        <?php
+        /**
+         * Plugin Name: Contact-Zaicha
+         */
+
+        function contact_form()
+        {
+            $content = '';
+            $content .= '<h2>Contact Us!</h2>';
+        $content .= '<form method="POST" action="#">';
+
+
+        if (get_option("name") ==1){
+            $content .= '<label for="Your Name">Your Name</label>';
+            $content .= '<input type ="text" name="Your_Name" class= "form-control" placeholder="Enter Your Name "/>';
+            }
+
+
+        if(get_option("email") ==1) {
+            $content .= '<label for="Your Email"> Your Email </label>';
+            $content .= '<input type ="email" name="YourEmail" class="form-control" placeholder="Enter Your Email"/>';
+        }
+        
+            if(get_option("number") ==1){
+            $content .= '<label for="Your Phone Number">Your Phone Number </label>';
+            $content .= '<input type ="tél" name="Number" class="form-control" placeholder="Enter Your Phone Number"/>';
+            } 
+
+
+            if(get_option("Comments") ==1){
+            $content .= '<label for="Your Questions Or Your Comments">Your Questions Or Your Comments </label>';
+            $content .= '<textarea name="Your_Comments" placeholder="Your Questions Or Your Comments" class="form-control"></textarea>';
+            }
+            
+
+            $content .= '<br/><input type="submit" name="edit_form" class="btn btn-dark" value="Submit" />';
+
+
+        
+        $content .= '</form>';    
+            
+            
+        
+            return $content;
+
+        }
+        add_shortcode('plugin_contact','contact_form');
+
+        if (isset($_POST['edit_form'])) {
+            $YourName = $_POST['Your_Name'];
+            $YourEmail = $_POST['YourEmail'];
+            // $YourPhoneNumber = $_POST['Your_Phone_Number'];
+            
+            // $YourQuestions= $_POST['Your_Comments'];
+
+            //table name: 'formcontact'
+            global $wpdb;
+             $wpdb->insert('formcontact', array('Name' => $YourName, 'Email' => $YourEmail, 'Number' => $_POST['Number'], 'Comments' => $_POST['Your_Comments']  ));
+        }
+
+        function wporg_options_page() {
+            add_menu_page(
+                'Contact_form',
+                'Contact form ',
+                'manage_options',
+                plugin_dir_path(__FILE__) . 'admin/admin.php',
+                null,
+                'dashicons-forms',
+                100
+            );
+
+            //add_submenu_page( string $parent_slug, string $page_title, string $menu_title, string $capability, string $menu_slug, callable $function = '', int $position = null )
+        }
+        //admin settings options
+        add_action( 'admin_menu', 'wporg_options_page' );
+
+
+
+        ?>
